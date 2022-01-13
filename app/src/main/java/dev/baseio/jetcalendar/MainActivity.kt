734407fb24a7c079608a72a3ea40e5aa51204944
Anmol Verma <anmol.verma4@gmail.com>
2022-01-13 15:56:01 +0530

    implemented clear selection

--- app/src/main/java/dev/baseio/jetcalendar/MainActivity.kt
+++ app/src/main/java/dev/baseio/jetcalendar/MainActivity.kt
@@ -3,16 +3,20 @@ package dev.baseio.jetcalendar
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Clear
 import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import dev.baseio.jetcalendar.ui.theme.JetCalendarSampleTheme
 import dev.baseio.libjetcalendar.JetCalendar
 import dev.baseio.libjetcalendar.data.JetDay
-import kotlinx.coroutines.flow.MutableStateFlow
 
 class MainActivity : ComponentActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,9 +24,7 @@ class MainActivity : ComponentActivity() {
     setContent {
       JetCalendarSampleTheme {
         // A surface container using the 'background' color from the theme
-        Surface(color = MaterialTheme.colors.background) {
-          CalendarExample()
-        }
+        CalendarExample()
       }
     }
   }
@@ -31,12 +33,33 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun CalendarExample() {
   var stateFlow by rememberSaveable { mutableStateOf(setOf<JetDay>()) }
-  JetCalendar(modifier = Modifier, onDateSelected = { jetDay ->
-    stateFlow = hashSetOf<JetDay>().apply {
-      addAll(stateFlow)
-      add(jetDay)
+  Surface(color = MaterialTheme.colors.background) {
+    Scaffold(topBar = {
+      TopAppBar(title = {
+        Text("Calendar Example")
+      }, actions = {
+        IconButton(modifier = Modifier.then(Modifier.padding(8.dp)),
+          onClick = {
+            stateFlow = hashSetOf()
+          }) {
+          Icon(
+            Icons.Filled.Clear,
+            "clear selection",
+            tint = Color.White
+          )
+        }
+      })
+    }) {
+      JetCalendar(modifier = Modifier, onDateSelected = { jetDay ->
+        stateFlow = hashSetOf<JetDay>().apply {
+          addAll(stateFlow)
+          add(jetDay)
+        }
+      }, selectedDates = stateFlow)
     }
-  }, selectedDates = stateFlow)
+  }
+
+
 }
 
 @Preview(showBackground = true)
